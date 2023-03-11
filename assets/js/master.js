efa1c7a51ba85a3415dc8e2b38e28336c49f447d
abbasebadian <abbasebadiann@gmail.com>
2023-03-11 23:43:08 +0330

    acc fix

--- assets/js/master.js
+++ assets/js/master.js
@@ -5,13 +5,27 @@ document.onreadystatechange = function () {
     const $main = document.querySelector('#main-wrapper');
     const $toggler = document.querySelector('.bi-list');
     const $closer = document.querySelector('.sidebar .bi-x');
+    const $sidebarButton = document.querySelectorAll('.accordion-header');
+    const $show = document.querySelectorAll('.accordion-collapse');
     
     $toggler && $toggler.addEventListener('click', function (e) {
         $main && $main.classList.toggle('collapsed');
+        console.log($show )
+        for(let button of $show)
+            button.classList.remove('show'); 
     })
     
     $closer && $closer.addEventListener('click', function (e) {
         $main && $main.classList.remove('collapsed');
+        for(let button of $show)
+            button.classList.remove('show'); 
     })
+
+    for(let button of $sidebarButton)
+        button.addEventListener('click', function (e) {
+            $main && $main.classList.remove('collapsed');  
+        })
+
+
     
 }
